9e907234fa7a8e9e84c04aa33d7033641c6a806f
Jonathan <Jonathan_Pusparajah@hotmail.com>
2020-11-20 11:34:30 +0100

    redoing opsgenie2

--- opsgenie.tf
+++ opsgenie.tf
@@ -11,7 +11,7 @@ data "opsgenie_user" "opsgenie_owner" {
 }
 */
 
-/*resource "opsgenie_user" "user1" {
+resource "opsgenie_user" "user1" {
   username = "lotnob99@student.kristiania.no"
   full_name = "Lottie Noble"
   role = "User"
@@ -59,4 +59,4 @@ resource "opsgenie_schedule_rotation" "teamrotation" {
     type = "user"
     id = opsgenie_user.user2.id
   }
-}*/
+}
