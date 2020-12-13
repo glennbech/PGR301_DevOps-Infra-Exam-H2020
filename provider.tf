8882527e5ea5ca284f13d9ff600a25b1c458277b
glennbech <glenn.bech@gmail.com>
2020-12-13 21:17:00 +0100

    new provider config

--- provider.tf
+++ provider.tf
@@ -1,14 +1,14 @@
 terraform {
   backend "gcs" {
-    bucket = "pgr301-devops-infra-bucket_02"
-    prefix = "terraformstate"
+    bucket = "pgr301-terraform-state"
+    prefix = "terraformstate-10007"
     credentials = "google-key.json"
   }
 }
 
 provider "google-beta" {
   credentials = file("google-key.json")
-  project = "pgr301-devops-exam-monster"
+  project = "terraform-292215"
   version = "~> 3.0.0-beta.1"
 }
 
